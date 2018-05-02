14279eaf48206c37df43931b69ba3aa695c193d0
Tammo Rukat <tammorukat@gmail.com>
2018-05-02 10:42:38 +0100

    testing int overflow in lbda updates

--- lom/_numba/lambda_updates_numba.py
+++ lom/_numba/lambda_updates_numba.py
@@ -222,10 +222,10 @@ def make_lbda_update_fct(model, dimensionality):
             # correct predictions, counting 0 prediction as false
             P = counter(*[x.val for x in parm.layer.factors], parm.layer.child())
             # number of data points that are to be predicted
-            ND = np.prod(parm.layer.child().shape) - np.sum(parm.layer.child() == 0)
+            ND = np.prod(parm.layer.child().shape) - np.count_nonzero(parm.layer.child() == 0)
             parm.val = np.max([-np.log(((ND + alpha + beta) / (float(P) + alpha)) - 1), 0])
 
-            print('\n')
-            print(P, ND)
+            # print('\n')
+            # print(P, ND)
 
         return lbda_update_fct
